52197a7861ae1cb55e178398f4566860ac27c31b
yodelingclam <zack.ogilvie@gmail.com>
2018-04-18 15:06:21 -0500

    ask on home page

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 $userId = null;
 $userPic = null;
 require 'connect.php';
+$query = "SELECT * FROM course ORDER BY CourseAbv";
+$statement = $db->prepare($query);	
+$statement -> execute(); 
+$courses = $statement->fetchAll();
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,94 +21,130 @@ require 'connect.php';
 		<div class="row">
 			<div id="redditThing" class="column">
 				<script src='https://redditjs.com/subreddit.js' data-subreddit='programming' data-theme='dark' ></script>
-			</div>
-			<div class="column">
-				<?php 
-				$query = "SELECT * FROM question JOIN users USING(UserId) JOIN course USING(CourseId) ORDER BY TimeStamp DESC";
-				$statement = $db->prepare($query);	
-				$statement -> execute(); 
-				$questions = $statement->fetchAll();
-				?>
-				<div class="mainUl" style="width: auto;">
-					<?php foreach ($questions as $key => $question): ?>
-						<div class="mainLi" style="width: auto;">
-							<div class="question" style="width: auto;">
-								<?php $pic = 'images/users/'.$question["ProfilePicURL"] ?>
-								<a href="question.php?post=<?= $question['QuestionId'] ?>&course=<?= $question['CourseName'] ?>&title=<?= $question['Title'] ?>"><h1><?= $question['Title'] ?></h1></a>
-								<h3><?= $question['FName'].' '.$question['LName'] ?></h3>
-								<p style="display: inline;"><?= $question['TimeStamp'] ?></p>
-								<p style="margin: 0px;">Course: <?= $question['CourseName'] ?></p>
-								<hr>
-								<p><?= $question['Content'] ?></p>
-								<?php if ((isset($_SESSION['admin']) && $_SESSION['admin'] > 0) || (isset($_SESSION['userId']) && $_SESSION['userId'] == $question['UserId'])): ?>
-									<a href="question.php?post=<?= $question['QuestionId'] ?>&edit">Edit</a>
-									<a href="#" onclick="$('#answer<?=$question['QuestionId']?>').show();">Answer</a>
-								<?php endif ?>
-								<?php if (isset($_SESSION['admin']) && $_SESSION['admin'] > 1): ?>
-									<a href="#" onclick="
-									$.ajax({
-										async: true,
-										url: 'delete.php',
-										type: 'POST',
-										data: { question: <?= $question['QuestionId'] ?> },
-										success: function(){location.reload();} 
-									});
-									">Delete</a>
-								<?php endif ?>
-							</div>
+				<form style="width: 100%; margin: auto;" id="newPost" method="post" action="submitPost.php">
+					<fieldset>
+						<input id="title" name="title" type="text" placeholder="Title of question" required autofocus/><br>
+						<textarea id="editor" name="content" rows='10' style="width: 100%; height: 250px;"></textarea><br>
+						<label for="course">Course:</label>
+						<select id="course" name="course">
+							<?php foreach ($courses as $key => $value): ?>
+								<option value="<?= $value['CourseId'] ?>"> <?= $value['CourseAbv'] .' - '. $value['CourseName']?></option>
+							<?php endforeach ?>
+						</select>
 
+						<?php 
+						require_once 'securimage/securimage.php';
+						echo "<div id='captcha_container_1'>\n";
+						echo Securimage::getCaptchaHtml();
+						echo "\n</div>\n"; 
+						?>
 
-							<div id="answer<?=$question['QuestionId']?>" style="display: none;">
-								<form action="newAnswer.php" method="post" style="width: 90%;">
-									<textarea id="editor<?=$question['QuestionId']?>" name="content" style="width: 50%; height: 250px;"></textarea><br>
-									<?php 
-									require_once 'securimage/securimage.php';
-									echo "<div id='captcha_container_1'>\n";
-									echo Securimage::getCaptchaHtml();
-									echo "\n</div>\n"; 
-									?>
-									<button type="submit">Submit</button><br>
-									<button type="button" onclick="$('#answer<?=$question['QuestionId']?>').hide();">Cancel</button>
-									<input type="hidden" name="questionid" value="<?=$question['QuestionId']?>">
-								</form>
-								<script>
-									ClassicEditor
-									.create( document.querySelector( '#editor<?=$question['QuestionId']?>' ) )
-									.catch( error => {
-										console.error( error );
-									} );
-								</script>
-							</div>
-
-							<div id="answers" style="width: auto;">
-								<?php
-								$query = "SELECT * FROM answer JOIN users USING(UserId) WHERE QuestionId = :questionid AND AnswerId = (SELECT Selected FROM question WHERE QuestionId = :questionid)";
-								$statement = $db->prepare($query);
-								$statement -> bindValue(":questionid", $question['QuestionId']);
-								$statement -> execute(); 
-								$answer = $statement->fetch();
-								?>
-								<?php if ($answer != null): ?>
-									<?php $answerPic = 'images/users/'.$answer["ProfilePicURL"] ?>
-									<div>
-										<img class="answerPic" src=<?= $answerPic ?> alt="profile picture" onerror="this.onerror=null; this.src='images/users/default.jpg';" width="75" height="75">
-										<div class="answer">
-
-											<h3><a href="#"><?= $answer['FName'].' '.$answer['LName'] ?></a></h3>
-											<img src="images/check.png" alt="chosen" width="35" height="35">
-
-											<p><?= $answer['Content'] ?></p>
-										</div>
-									</div>
-								<?php endif ?>
-							</div>
+						<button type="submit">Submit</button><br>
+						<button type="reset">Reset</button>
+					</fieldset>
+					<script>
+						ClassicEditor
+						.create( document.querySelector( '#editor' ) )
+						.catch( error => {
+							console.error( error );
+						} );
+					</script>
+				</form>
+				<style>
+				.ck-editor__editable
+				{
+					height: 150px;
+					width: 100%;
+				}
+			</style>
+		</div>
+		<div class="column">
+			<?php 
+			$query = "SELECT * FROM question JOIN users USING(UserId) JOIN course USING(CourseId) ORDER BY TimeStamp DESC";
+			$statement = $db->prepare($query);	
+			$statement -> execute(); 
+			$questions = $statement->fetchAll();
+			?>
+			<div class="mainUl" style="width: auto;">
+				<?php foreach ($questions as $key => $question): ?>
+					<div class="mainLi" style="width: auto;">
+						<div class="question" style="width: auto;">
+							<?php $pic = 'images/users/'.$question["ProfilePicURL"] ?>
+							<a href="question.php?post=<?= $question['QuestionId'] ?>&course=<?= $question['CourseName'] ?>&title=<?= $question['Title'] ?>"><h1><?= $question['Title'] ?></h1></a>
+							<h3><?= $question['FName'].' '.$question['LName'] ?></h3>
+							<p style="display: inline;"><?= $question['TimeStamp'] ?></p>
+							<p style="margin: 0px;">Course: <?= $question['CourseName'] ?></p>
+							<hr>
+							<p><?= $question['Content'] ?></p>
+							<?php if ((isset($_SESSION['admin']) && $_SESSION['admin'] > 0) || (isset($_SESSION['userId']) && $_SESSION['userId'] == $question['UserId'])): ?>
+								<a href="question.php?post=<?= $question['QuestionId'] ?>&edit">Edit</a>
+								<a href="#" onclick="$('#answer<?=$question['QuestionId']?>').show();">Answer</a>
+							<?php endif ?>
+							<?php if (isset($_SESSION['admin']) && $_SESSION['admin'] > 1): ?>
+								<a href="#" onclick="
+								$.ajax({
+									async: true,
+									url: 'delete.php',
+									type: 'POST',
+									data: { question: <?= $question['QuestionId'] ?> },
+									success: function(){location.reload();} 
+								});
+								">Delete</a>
+							<?php endif ?>
 						</div>
-						<br>
-					<?php endforeach ?>
-				</div>
+
+
+						<div id="answer<?=$question['QuestionId']?>" style="display: none;">
+							<form action="newAnswer.php" method="post" style="width: 90%;">
+								<textarea id="editor<?=$question['QuestionId']?>" name="content" style="width: 50%; height: 250px;"></textarea><br>
+								<?php 
+								require_once 'securimage/securimage.php';
+								echo "<div id='captcha_container_1'>\n";
+								echo Securimage::getCaptchaHtml();
+								echo "\n</div>\n"; 
+								?>
+								<button type="submit">Submit</button><br>
+								<button type="button" onclick="$('#answer<?=$question['QuestionId']?>').hide();">Cancel</button>
+								<input type="hidden" name="questionid" value="<?=$question['QuestionId']?>">
+							</form>
+							<script>
+								ClassicEditor
+								.create( document.querySelector( '#editor<?=$question['QuestionId']?>' ) )
+								.catch( error => {
+									console.error( error );
+								} );
+							</script>
+						</div>
+
+						<div id="answers" style="width: auto;">
+							<?php
+							$query = "SELECT * FROM answer JOIN users USING(UserId) WHERE QuestionId = :questionid AND AnswerId = (SELECT Selected FROM question WHERE QuestionId = :questionid)";
+							$statement = $db->prepare($query);
+							$statement -> bindValue(":questionid", $question['QuestionId']);
+							$statement -> execute(); 
+							$answer = $statement->fetch();
+							?>
+							<?php if ($answer != null): ?>
+								<?php $answerPic = 'images/users/'.$answer["ProfilePicURL"] ?>
+								<div>
+									<img class="answerPic" src=<?= $answerPic ?> alt="profile picture" onerror="this.onerror=null; this.src='images/users/default.jpg';" width="75" height="75">
+									<div class="answer">
+
+										<h3><a href="#"><?= $answer['FName'].' '.$answer['LName'] ?></a></h3>
+										<img src="images/check.png" alt="chosen" width="35" height="35">
+
+										<p><?= $answer['Content'] ?></p>
+									</div>
+								</div>
+							<?php endif ?>
+						</div>
+					</div>
+					<br>
+				<?php endforeach ?>
 			</div>
 		</div>
 	</div>
+</div>
 </body>
 </html>
 <script>
--- menu.php
+++ menu.php
@@ -39,64 +39,64 @@ crossorigin="anonymous"></script>
                 </div>
                 <div style="clear: both;"></div>
             </div> -->
-        <!-- </li> -->
-    <?php endif ?>
-    <li class="separator"></li>
-    <li><a href="#">Browse</a>
-        <div class="drop decor3_2" >
-            <div class="left">
-                <a href="browse.php?type=search&by=">Search</a><br />
-                <a href="browse.php?type=course">Browse by Course</a><br />
-                <!-- <a href="browse.php?type=tag">Browse by Tag</a><br /> -->
-            </div>
-            <div style="clear: both;"></div>
-        </div>
-    </li>
-    <?php if (!isset($_SESSION['userId'])): ?>
-        <li class="separator"></li>
-        <li>
-            <button onclick="myFunction()">Register</button>
-        </li>
-    <?php endif ?>
-    <li class="separator"></li> 
-    <?php $userId = (isset($_SESSION['userName']) ?  '<a href="#">'.$_SESSION["userName"].'</a>' : '<form action="login.php" method="post"><button type="submit">Login</button></li><li><input type="text" placeholder="Email" name="username" required><input type="password" placeholder="Password" name="password" required></li></form><form action="register.php" method="post"></form>' ) ?>
-    <li><?= $userId ?></a>
-        <?php if(isset($_SESSION['userName'])) : ?>
-            <?php $userPic = $_SESSION['userPic']?>
-            <div class="drop decor3_2 dropToLeft" style="width: auto;">
-                <?php include 'profileWidget.php'; ?>
-            </div>
+            <!-- </li> -->
         <?php endif ?>
-    </li>
-</ul>
-<?php if (!isset($_SESSION['userId'])): ?>
-    <div id="my-div" style="display:none;">
-        <script type="text/javascript" src="scripts/imagePreview.js"></script>
-        <form id="register" method="post" action="registerData.php" enctype="multipart/form-data">
-            <fieldset>
-                <label for="email">Email:</label>
-                <input id="email" name="email" type="email" placeholder="you@site.com" autofocus required/>
-                <br>
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" required />
-                <br>
-                <label for="password2">Confirm Password:</label>
-                <input type="password" name="password2" id="password2" required />
-                <br>
-                <label for="firstname">First Name:</label>
-                <input id="firstname" name="firstname" type="text" placeholder="First Name" required/>
-                <br>
-                <label for="lastname">First Name:</label>
-                <input id="lastname" name="lastname" type="text" placeholder="Last Name" required/>
-                <br>
-                <label for="ImgControl">Profile Picture</label>
-                <input id="ImgControl" type="file" name="image" accept="image/*">
-                <div id="ImgContain" style="display: none; width: 300px; height: 300px;"><img src="#" id="ImgPreview" alt="alt" style="display: none; width: 100%; height: 100%; object-fit: contain; /*magic*/"></div>
-                <br>
-                <button type="submit">Submit</button>
-                <button type="reset" onclick="hideImage();document.getElementById('my-div').style.display = 'none';">Cancel</button>
-            </fieldset>
-        </form>
-    </div>
-<?php endif ?>
-<div id="clear"> </div>
+        <li class="separator"></li>
+        <li><a href="#">Browse</a>
+            <div class="drop decor3_2" >
+                <div class="left">
+                    <a href="browse.php?type=search&by=">Search</a><br />
+                    <a href="browse.php?type=course">Browse by Course</a><br />
+                    <!-- <a href="browse.php?type=tag">Browse by Tag</a><br /> -->
+                </div>
+                <div style="clear: both;"></div>
+            </div>
+        </li>
+        <?php if (!isset($_SESSION['userId'])): ?>
+            <li class="separator"></li>
+            <li>
+                <button onclick="myFunction()">Register</button>
+            </li>
+        <?php endif ?>
+        <li class="separator"></li> 
+        <?php $userId = (isset($_SESSION['userName']) ?  '<a href="#">'.$_SESSION["userName"].'</a>' : '<form action="login.php" method="post"><button type="submit">Login</button></li><li><input type="text" placeholder="Email" name="username" required><input type="password" placeholder="Password" name="password" required></li></form><form action="register.php" method="post"></form>' ) ?>
+        <li><?= $userId ?>
+            <?php if(isset($_SESSION['userName'])) : ?>
+                <?php $userPic = $_SESSION['userPic']?>
+                <div class="drop decor3_2 dropToLeft" style="width: auto;">
+                    <?php include 'profileWidget.php'; ?>
+                </div>
+            <?php endif ?>
+        </li>
+    </ul>
+    <?php if (!isset($_SESSION['userId'])): ?>
+        <div id="my-div" style="display:none;">
+            <script type="text/javascript" src="scripts/imagePreview.js"></script>
+            <form id="register" method="post" action="registerData.php" enctype="multipart/form-data">
+                <fieldset>
+                    <label for="email">Email:</label>
+                    <input id="email" name="email" type="email" placeholder="you@site.com" autofocus required/>
+                    <br>
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password" required />
+                    <br>
+                    <label for="password2">Confirm Password:</label>
+                    <input type="password" name="password2" id="password2" required />
+                    <br>
+                    <label for="firstname">First Name:</label>
+                    <input id="firstname" name="firstname" type="text" placeholder="First Name" required/>
+                    <br>
+                    <label for="lastname">First Name:</label>
+                    <input id="lastname" name="lastname" type="text" placeholder="Last Name" required/>
+                    <br>
+                    <label for="ImgControl">Profile Picture</label>
+                    <input id="ImgControl" type="file" name="image" accept="image/*">
+                    <div id="ImgContain" style="display: none; width: 300px; height: 300px;"><img src="#" id="ImgPreview" alt="alt" style="display: none; width: 100%; height: 100%; object-fit: contain; /*magic*/"></div>
+                    <br>
+                    <button type="submit">Submit</button>
+                    <button type="reset" onclick="hideImage();document.getElementById('my-div').style.display = 'none';">Cancel</button>
+                </fieldset>
+            </form>
+        </div>
+    <?php endif ?>
+    <div id="clear"> </div>
